517455ff45a3cfaef2fa0357da9e15b36da3297e
tinol22 <56993560+tinol22@users.noreply.github.com>
2020-11-14 18:41:55 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('quiz.urls')),
+    path('api/auth/', include('accounts.urls')),
+    path('nested_admin', include('nested_admin.urls')),
+]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
